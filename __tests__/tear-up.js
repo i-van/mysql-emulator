63e8a4d8518681d36efc9ffe5fa73e710dca757a
Ivan Nosov <i.k.nosov@gmail.com>
2023-04-24 13:55:25 -0700

    fix: shutdown connection once tests are done

--- __tests__/tear-up.js
+++ __tests__/tear-up.js
@@ -2,17 +2,20 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 let connection;
-
 require('../src').query = async (sql, params = []) => {
-  if (!connection) {
-    connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
-  }
-
   const [rows, fields] = await connection.query(sql, params);
   return rows;
 };
+
+beforeAll(async () => {
+  connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+});
+
+afterAll(async () => {
+  await connection.close();
+});
