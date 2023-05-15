293a54c553af5f4433ccdfbadefde9af31792ce6
Ivan Nosov <i.k.nosov@gmail.com>
2023-05-15 12:38:28 -0700

    feat: try to cast non-integer values to integer

--- __tests__/e2e/update.spec.ts
+++ __tests__/e2e/update.spec.ts
@@ -2,7 +2,7 @@ import { query } from '../../src';
 
 describe('update', () => {
   beforeEach(async () => {
-    await query(`CREATE TABLE books (id int, name varchar(255), pages int)`);
+    await query(`CREATE TABLE books (id int, name varchar(255), pages int unsigned)`);
     await query(`INSERT INTO books (id, name, pages) VALUES (1, 'name1', 100)`);
     await query(`INSERT INTO books (id, name, pages) VALUES (2, 'name2', 300)`);
     await query(`INSERT INTO books (id, name, pages) VALUES (3, 'name3', 500)`);
@@ -43,6 +43,31 @@ describe('update', () => {
       { id: 1, name: 'name1', pages: 101 },
       { id: 2, name: 'name2', pages: 301 },
       { id: 3, name: 'name3', pages: 501 },
+    ]);
+  });
+  it('should throw an error if pages is string', async () => {
+    try {
+      await query(`UPDATE books b SET b.pages = 'many'`);
+    } catch (err: any) {
+      expect(err.message).toEqual(`Incorrect integer value: 'many' for column 'pages' at row 1`);
+    }
+  });
+  it('should throw an error if pages is negative', async () => {
+    try {
+      await query(`UPDATE books b SET b.pages = -100`);
+    } catch (err: any) {
+      expect(err.message).toEqual(`Out of range value for column 'pages' at row 1`);
+    }
+  });
+  it('should cast string pages to integer', async () => {
+    const res = await query(`UPDATE books b SET b.pages = '100'`);
+    const rows = await query(`SELECT * from books`);
+
+    expect(res.affectedRows).toEqual(3);
+    expect(rows).toEqual([
+      { id: 1, name: 'name1', pages: 100 },
+      { id: 2, name: 'name2', pages: 100 },
+      { id: 3, name: 'name3', pages: 100 },
     ]);
   });
 });
--- src/query-runner/update.processor.ts
+++ src/query-runner/update.processor.ts
@@ -1,7 +1,8 @@
-import { Server } from '../server';
+import { Column, Server } from '../server';
 import { UpdateQuery } from '../parser';
 import { mapKeys } from '../utils';
 import { Evaluator } from './evaluator';
+import { ProcessorError } from './processor-error';
 
 export class UpdateProcessor {
   constructor(protected server: Server) {}
@@ -9,21 +10,43 @@ export class UpdateProcessor {
   process(query: UpdateQuery) {
     const table = this.server.getDatabase(query.database).getTable(query.table);
     const columns = table.getColumns().map(c => `${query.table}::${c.getName()}`);
+    const columnDefinitions = table.getColumns();
+    const columnDefinitionMap = new Map<string, Column>(
+      columnDefinitions.map((c) => [c.getName(), c])
+    );
     const evaluator = new Evaluator(this.server, columns);
+    const getColumnDefinition = (column: string): Column => {
+      const c = columnDefinitionMap.get(column);
+      if (!c) {
+        throw new ProcessorError(`Unknown column '${column}' in 'field list'`);
+      }
+      return c;
+    };
 
     let affectedRows = 0;
     const updatedRows = table.getRows().map(r => {
       const row = mapKeys(r, (key) => `${query.table}::${key}`);
-      const update = query.where === null || evaluator.evaluateExpression(query.where, row);
-      if (update) {
-        affectedRows++;
-        return query.assignments.reduce((res, a) => ({
-          ...res,
-          [a.column]: evaluator.evaluateExpression(a.value, row),
-        }), r);
-      } else {
+      const needsUpdate = query.where === null || evaluator.evaluateExpression(query.where, row);
+      if (!needsUpdate) {
         return r;
       }
+
+      affectedRows++;
+      return query.assignments.reduce((res, a) => {
+        const column = getColumnDefinition(a.column);
+        const value = evaluator.evaluateExpression(a.value, row);
+        try {
+          return {
+            ...res,
+            [column.getName()]: column.cast(value),
+          }
+        } catch (err: any) {
+          if (['OUT_OF_RANGE_VALUE', 'INCORRECT_INTEGER_VALUE'].includes(err.code)) {
+            throw new ProcessorError(`${err.message} at row ${affectedRows}`);
+          }
+          throw err;
+        }
+      }, r);
     });
     table.setRows(updatedRows);
 
